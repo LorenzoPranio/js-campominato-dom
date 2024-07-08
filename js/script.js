1b3fcb4df3c74228ee54c8593463c0a32c3d6dfc
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-08 14:17:20 +0200

    [UPD]Added generate_bombs function in script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
 //Recupero il pulsante dal dom
-
 const btn = document.getElementById('btn-start');
 
 //Recupero l'elemento della griglia
-
 const grid = document.getElementById('grid');
+const total_cells = 100;
+
+//Devo generare 16 bombe e inserirle nei quadrati in modo randomico
+const max_bombs = 16;
+const bombs = [];
+
+//Funzione delle bombe
+function generate_bombs() {
+    while (bombs.length < max_bombs) {
+        const bombs_position = Math.floor(Math.random() * total_cells);
+        if (bombs.includes(bombs_position)) {
+            bombs.push(bombs_position);
+        }
+    }
+    return bombs;
+}
 
 //Assegno l'evento click al pulsante recuperato nel dom 
-
 btn.addEventListener('click', function(){
     grid.innerHTML = "";
     //Eseguo un ciclo for per la grglia 10x10
@@ -21,28 +34,19 @@ btn.addEventListener('click', function(){
             this.classList.toggle('clicked-lightblue')
         })
     //Metto il numero progressivo al quadrato
-
     current_square.innerText = i + 1;
-
     //Appendo il quadrato creato nella griglia
-
     grid.append(current_square);
     }
 })
 
 //Definisco la funzione che mi crea i quadrati nella griglia
 function create_square(){
-
     let current_element = document.createElement('div');
-
     //Assegnazione della classe square all'elemento creato
-
     current_element.classList.add('square')
-
     //restituisco il quadrato nella variabile current element
-
     return current_element;
-
 }
 
 
